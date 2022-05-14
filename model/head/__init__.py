6aefd3f2726a1972e80e46faf21285bd182fcb94
Weiyang Liu <eulerliu@gmail.com>
2022-05-13 17:03:47 -0700

    Update __init__.py

--- model/head/__init__.py
+++ model/head/__init__.py
@@ -5,4 +5,5 @@ from .cosface import CosFace
 from .arcface import ArcFace
 from .sphereface2 import SphereFace2
 from .spherefaceplus import SphereFacePlus
+from .gasoftmaxloss import GAsoftmax
 from .spherefacer import SphereFaceR_N, SphereFaceR_H, SphereFaceR_S
